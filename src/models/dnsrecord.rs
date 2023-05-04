873154178dee0e8fbee2b29954ea012f1b740b0e
sissto <login+github@sissto.com>
2023-05-04 17:18:39 +0200

    Added clone trait to dnszone and dnsrecord struct

--- src/models/dnsrecord.rs
+++ src/models/dnsrecord.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct DnsRecord {
     pub id: String,
     pub hostname: String,
